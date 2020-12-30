b517dc0e0f6edd1f0198884ab0db72af283c8f55
sdk-team <sdk-team@alibabacloud.com>
2020-12-30 08:15:13 +0000

    Generated 2019-02-25 for sdk-test.

--- aliyun-python-sdk-sdk-test/setup.py
+++ aliyun-python-sdk-sdk-test/setup.py
@@ -31,7 +31,7 @@ Created on 7/3/2015
 """
 
 PACKAGE = "aliyunsdksdk_test"
-NAME = "aliyun-python-sdk-sdk-test_test"
+NAME = "aliyun-python-sdk-sdk-test-test"
 DESCRIPTION = "The sdk-test module of Aliyun Python sdk."
 AUTHOR = "Aliyun"
 AUTHOR_EMAIL = "aliyun-developers-efficiency@list.alibaba-inc.com"
